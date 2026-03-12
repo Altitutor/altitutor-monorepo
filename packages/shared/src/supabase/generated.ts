13e8d06ab047856f948289de7cef8575f2819095
Matthew Chua <matt@altitutor.com>
2026-03-12 15:36:17 +1030

    chore: regenerate types

--- packages/shared/src/supabase/generated.ts
+++ packages/shared/src/supabase/generated.ts
@@ -7,11 +7,6 @@ export type Json =
   | Json[]
 
 export type Database = {
-  // Allows to automatically instantiate createClient with right options
-  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
-  __InternalSupabase: {
-    PostgrestVersion: "13.0.5"
-  }
   public: {
     Tables: {
       activity_events: {
@@ -1346,6 +1341,91 @@ export type Database = {
           },
         ]
       }
+      credit_balance_transactions: {
+        Row: {
+          amount_cents: number
+          created_at: string
+          credit_note_id: string | null
+          credit_type: string | null
+          currency: string
+          debit_type: string | null
+          description: string | null
+          effective_at: string
+          id: string
+          invoice_id: string | null
+          raw: Json
+          stripe_credit_balance_transaction_id: string
+          stripe_credit_grant_id: string
+          stripe_customer_id: string | null
+          stripe_invoice_id: string | null
+          stripe_invoice_line_item_id: string | null
+          type: string
+          updated_at: string
+        }
+        Insert: {
+          amount_cents: number
+          created_at?: string
+          credit_note_id?: string | null
+          credit_type?: string | null
+          currency?: string
+          debit_type?: string | null
+          description?: string | null
+          effective_at: string
+          id?: string
+          invoice_id?: string | null
+          raw: Json
+          stripe_credit_balance_transaction_id: string
+          stripe_credit_grant_id: string
+          stripe_customer_id?: string | null
+          stripe_invoice_id?: string | null
+          stripe_invoice_line_item_id?: string | null
+          type: string
+          updated_at?: string
+        }
+        Update: {
+          amount_cents?: number
+          created_at?: string
+          credit_note_id?: string | null
+          credit_type?: string | null
+          currency?: string
+          debit_type?: string | null
+          description?: string | null
+          effective_at?: string
+          id?: string
+          invoice_id?: string | null
+          raw?: Json
+          stripe_credit_balance_transaction_id?: string
+          stripe_credit_grant_id?: string
+          stripe_customer_id?: string | null
+          stripe_invoice_id?: string | null
+          stripe_invoice_line_item_id?: string | null
+          type?: string
+          updated_at?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "credit_balance_transactions_credit_note_id_fkey"
+            columns: ["credit_note_id"]
+            isOneToOne: false
+            referencedRelation: "credit_notes"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "credit_balance_transactions_invoice_id_fkey"
+            columns: ["invoice_id"]
+            isOneToOne: false
+            referencedRelation: "invoices"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "credit_balance_transactions_invoice_id_fkey"
+            columns: ["invoice_id"]
+            isOneToOne: false
+            referencedRelation: "vstudent_invoices"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       credit_notes: {
         Row: {
           amount_cents: number
@@ -1973,6 +2053,7 @@ export type Database = {
           dispute_updated_at: string | null
           fee_cents: number | null
           finalized_at: string | null
+          has_credit_notes: boolean
           hosted_invoice_url: string | null
           id: string
           invoice_date: string
@@ -2012,6 +2093,7 @@ export type Database = {
           dispute_updated_at?: string | null
           fee_cents?: number | null
           finalized_at?: string | null
+          has_credit_notes?: boolean
           hosted_invoice_url?: string | null
           id?: string
           invoice_date: string
@@ -2051,6 +2133,7 @@ export type Database = {
           dispute_updated_at?: string | null
           fee_cents?: number | null
           finalized_at?: string | null
+          has_credit_notes?: boolean
           hosted_invoice_url?: string | null
           id?: string
           invoice_date?: string
@@ -5615,7 +5698,7 @@ export type Database = {
           discontinued_at?: string | null
           email?: string | null
           first_name: string
-          id?: string
+          id: string
           invite_token?: string | null
           last_name: string
           phone?: string | null
@@ -11683,6 +11766,10 @@ export type Database = {
     }
     Functions: {
       _format_date_ordinal: { Args: { ts: string }; Returns: string }
+      add_enum_value: {
+        Args: { enum_name: string; new_value: string }
+        Returns: undefined
+      }
       assign_staff_to_booking: {
         Args: {
           p_available_staff_ids: string[]
@@ -12010,7 +12097,9 @@ export type Database = {
         Args: { student_id: string }
         Returns: boolean
       }
+      is_adminstaff: { Args: never; Returns: boolean }
       is_adminstaff_active: { Args: never; Returns: boolean }
+      is_staff: { Args: never; Returns: boolean }
       is_student: { Args: never; Returns: boolean }
       is_tutor: { Args: never; Returns: boolean }
       is_ucat_student: { Args: never; Returns: boolean }
@@ -12043,8 +12132,6 @@ export type Database = {
         Args: { logged_by_student_id: string; operations: Json }
         Returns: Json
       }
-      map_day_to_number: { Args: { day_string: string }; Returns: number }
-      map_subject_to_id: { Args: { subject_code: string }; Returns: string }
       map_tutor_to_id: {
         Args: { first_name: string; last_name: string }
         Returns: string
@@ -12270,10 +12357,6 @@ export type Database = {
         }
         Returns: Json
       }
-      set_claim: {
-        Args: { claim: string; uid: string; value: Json }
-        Returns: undefined
-      }
       staff_full_name_lower: {
         Args: { p_first_name: string; p_last_name: string }
         Returns: string
@@ -12346,6 +12429,7 @@ export type Database = {
       tutor_ucat_upsert_mock:
         | {
             Args: {
+              p_instructions_text?: Json
               p_is_private: boolean
               p_mock_id: string
               p_name: string
@@ -12355,7 +12439,6 @@ export type Database = {
           }
         | {
             Args: {
-              p_instructions_text?: Json
               p_is_private: boolean
               p_mock_id: string
               p_name: string
@@ -12398,6 +12481,7 @@ export type Database = {
         Args: { logged_by_staff_id: string; operations: Json }
         Returns: Json
       }
+      user_role: { Args: never; Returns: string }
       validate_all_topic_codes: {
         Args: never
         Returns: {
@@ -12417,7 +12501,6 @@ export type Database = {
         }[]
       }
       validate_phone_e164: { Args: { phone: string }; Returns: boolean }
-      verify_email: { Args: { user_email: string }; Returns: undefined }
     }
     Enums: {
       billing_type: "CLASS" | "EXAM_COURSE" | "DRAFTING"
@@ -12611,3 +12694,4 @@ export const Constants = {
     },
   },
 } as const
+
